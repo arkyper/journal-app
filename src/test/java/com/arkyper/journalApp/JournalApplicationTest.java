7e7920b2581c50e06bf79e04a7452a08d15f0b63
arkyper.org <arkyper.org@gmail.com>
2025-05-11 06:32:03 +0000

    Creating Repo for this Project

---
+++ src/test/java/com/arkyper/journalApp/JournalApplicationTest.java
@@ -0,0 +1,12 @@
+package com.arkyper.journalApp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+public class JournalApplicationTest {
+    
+    @Test
+    void contextLoads(){};
+}
